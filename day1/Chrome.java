7909b973c676ab15bf5373578393c34f2c081a5e
Danam04-hub <mkdanam06@gmail.com>
2024-12-16 22:54:31 +0800

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	private static String isVisible;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float Version = 91.0f;
+		String Developer = "Google";
+		Boolean isBeta = true;
+		int releseYear = 2008;
+		char shortcutKey='c';
+
+		System.out.println("Version: "+Version);
+		System.out.println("Developer: "+Developer);
+		System.out.println("isVisible: "+isVisible);
+		System.out.println("releseYear: "+releseYear);
+		System.out.println("shortcutKey: "+shortcutKey);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	float browserVersion = 100.2f;
+	String browserName = "firefox";
+	Boolean isVisible = true;
+	int releseYear = 1998;
+	char browserLogo='f';
+	
+	System.out.println("Browser Name: "+browserName);
+	System.out.println("Browser Version: "+browserVersion);
+	System.out.println("Browser Logo:"+browserLogo);
+	System.out.println("Visible or not: "+isVisible);
+	System.out.println("releseYear: "+releseYear);
+	
+ 
+	}
+
+}
